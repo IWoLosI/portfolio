74a9fabb25186ed6f26e1baff31ce90a09693207
WoLos <wolos80@gmail.como>
2023-02-02 22:20:39 +0300

    add uber and pule in portfolio.mail working

--- src/js/script.js
+++ src/js/script.js
@@ -17,3 +17,70 @@ counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
 
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 600) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+$("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+});
+
+new WOW().init();
+
+// $('form', 'contacts__form').submit(function(e) {
+//     e.preventDefault();
+
+//     if (!$(this).valid()) {
+//         return;
+//     }
+    
+//     $.ajax({
+//         type: "POST",
+//         url: "mailer/smart.php",
+//         data: $(this).serialize()
+//     }).done(function() {
+//         $(this).find("input").val("");
+//         $('form').trigger('reset');
+//     });
+//     return false;
+// });
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
+
+
